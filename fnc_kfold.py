8474e34a6273ad5ee4115e162ee2e5987ae05a7a
SAID Abdelhakim <abdelhakim.said@commandersact.com>
2020-02-25 11:07:12 +0100

    changing dataset label to bi-stance

--- fnc_kfold.py
+++ fnc_kfold.py
@@ -1,7 +1,7 @@
-import sys
 import numpy as np
-
 from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.neural_network import MLPClassifier
+
 from feature_engineering import refuting_features, polarity_features, hand_features, gen_or_load_feats
 from feature_engineering import word_overlap_features
 from utils.dataset import DataSet
@@ -67,14 +67,14 @@ if __name__ == "__main__":
         X_test = Xs[fold]
         y_test = ys[fold]
 
-        clf = GradientBoostingClassifier(n_estimators=200, random_state=14128, verbose=True)
+        clf = GradientBoostingClassifier()
         clf.fit(X_train, y_train)
 
         predicted = [LABELS[int(a)] for a in clf.predict(X_test)]
         actual = [LABELS[int(a)] for a in y_test]
 
-        fold_score, _ = score_submission(actual, predicted)
-        max_fold_score, _ = score_submission(actual, actual)
+        fold_score = score_submission(actual, predicted)
+        max_fold_score = score_submission(actual, actual)
 
         score = fold_score/max_fold_score
 
